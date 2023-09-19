ccedd2ff97157538b42ab79eb4a690d7713fd2d7
KaitoMizukami <kaito.program@gmail.com>
2023-09-20 00:52:37 +0800

    Adaptor design pattern

---
+++ Adaptor1/Main.java
@@ -0,0 +1,9 @@
+package Adaptor1;
+
+public class Main {
+    public static void main(String[] args) {
+        Print p = new PrintBanner("Hello World!");
+        p.printWeak();
+        p.printStrong();
+    }
+}
---
+++ Adaptor2/Print.java
@@ -0,0 +1,6 @@
+package Adaptor2;
+
+public abstract class Print {
+    public abstract void printWeak();
+    public abstract void printStrong();
+}
